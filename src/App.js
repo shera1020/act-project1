c176d286bfd5349ce6b0750f7cdeec55e20623fe
shera1020 <52871541+shera1020@users.noreply.github.com>
2020-08-29 06:30:15 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Products</h1>
+      <div>
+     <h3>AA Batery</h3>
+     <h4>$2.99</h4>
+
+<button>Add to Cart</button>
+      </div>
+    </div>
+  );
+}
