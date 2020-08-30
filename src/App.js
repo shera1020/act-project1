50ab1335fc793e0e1b7194e74a62f55af5bbb0c8
shera1020 <52871541+shera1020@users.noreply.github.com>
2020-08-29 21:35:02 -0400

    changes in App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React,{useState} from "react";
 import "./styles.css";
 
 export default function App() {
+const[cart,setCart] = useState([]);
+const [products] = useState([
+{
+   name:"AA Battery",
+   cost: "$2.99",
+   image: "https://webobjects2.cdw.com/is/image/CDW/5499073?$product-main$"
+},
+{
+     name:"Blanket",
+     cost: "$19.99",
+
+     image:"https://images-na.ssl-images-amazon.com/images/I/614T7BVc5NL._AC_SL1500_.jpg"
+
+}
+]);
+const addToCart=(product) => {
+console.log("We are in add to cart")
+ setCart([...cart,product])
+}
   return (
     <div className="App">
+      <header><button>Go to Cart({cart.length})</button></header>
       <h1>Products</h1>
-      <div>
-     <h3>AA Batery</h3>
-     <h4>$2.99</h4>
-  <img src="https://webobjects2.cdw.com/is/image/CDW/5499073?$product-main$" alt="battery"/>
-<button>Add to Cart</button>
+      <div className = "products"> 
+      {products.map((product,index)=>(
+        <div  className = "product" key = {index}>
+     <h3>{product.name}</h3>
+     <h4>{product.cost}</h4>
+  <img src={product.image} alt = {product.name}/>
+<button onClick = {  () => addToCart(product)}>Add to  Cart</button>
       </div>
-    </div>
+      
+      ) )}
+</div>
+</div>
   );
-}
+      }
+  
+       
+      
+
+      
+  
+      
+     
+    
